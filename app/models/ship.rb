454e412e952bd3a58587f4e318f460b3364a0124
LeahB8 <leahboyd08@gmail.com>
2019-04-25 16:10:09 +0100

    Done

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(ship_params)
+    @name = ship_params[:name]
+    @type = ship_params[:type]
+    @booty = ship_params[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships.clear
+  end
+
 end
